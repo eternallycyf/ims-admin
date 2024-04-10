974bdc9cefa3a3ebd80dac995d2db6468ac7922b
eternallycyf <969475322@qq.com>
2024-04-10 15:52:14 +0800

    💄 style: edit text

--- src/pages/components/animate/views/inview/container.tsx
+++ src/pages/components/animate/views/inview/container.tsx
@@ -15,7 +15,7 @@ export function repeat<T>(value: T, n: number): T[] {
   return Array(n).fill(value)
 }
 
-const TEXT = 'SlashAdmin'
+const TEXT = 'ImsAdmin'
 interface Props {
   isText: boolean
   isMulti: boolean
@@ -41,7 +41,7 @@ export default function ContainerView({ isText, variant, isMulti }: Props) {
               </m.div>
             ))}
           </MotionContainer>
-          )
+        )
         : (
           <MotionContainer className="flex flex-col items-center gap-6">
             {imgs.map((img, index) => (
@@ -59,7 +59,7 @@ export default function ContainerView({ isText, variant, isMulti }: Props) {
               />
             ))}
           </MotionContainer>
-          )}
+        )}
     </div>
   )
 }
