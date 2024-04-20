193018ca778c486d6edea06bd1eaff9408b13ad0
eternallycyf <969475322@qq.com>
2024-04-21 02:23:46 +0800

    🐛 fix: menu toggle re-render

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { App as AntdApp } from 'antd'
 import { MotionLazy } from '@/components'
-import { AntdConfig, AppProvider } from '@/Application'
+import { AntdConfig } from '@/Application'
 import Router from '@/router'
 
 function App() {
   return (
     <AntdConfig>
-      <AppProvider prefixCls={import.meta.env.VITE_PREFIXCLS}>
-        <AntdApp className="antd-app h-full w-full">
-          <MotionLazy>
-            <Router />
-          </MotionLazy>
-        </AntdApp>
-      </AppProvider>
+
+      <AntdApp className="antd-app h-full w-full">
+        <MotionLazy>
+          <Router />
+        </MotionLazy>
+      </AntdApp>
+
     </AntdConfig>
   )
 }
